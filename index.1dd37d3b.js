382451726409a8da3ba3d200d61266644aca919b
Enital <117684114+Enital@users.noreply.github.com>
2023-01-19 16:40:59 +0000

    Deploying to gh-pages from @ yuriyaza/goit-team-project-01@645a87ed1d9e52ed79e7f9157db0a7f4c8f4de0c 🚀

---
+++ index.1dd37d3b.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".gallery-swiper",{pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.1dd37d3b.js.map
---
+++ index.60aad7cf.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".reviews-swiper",{pagination:{el:".swiper-pagination"}});
+//# sourceMappingURL=index.60aad7cf.js.map
