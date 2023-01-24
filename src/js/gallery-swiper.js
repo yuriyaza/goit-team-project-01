afd6250ab2529e0b297408b31a2e809ca30c1287
Enital <117684114+Enital@users.noreply.github.com>
2023-01-24 11:20:36 +0200

    ++
    ще правки

--- src/js/gallery-swiper.js
+++ src/js/gallery-swiper.js
@@ -3,17 +3,9 @@ var swiper = new Swiper('.gallery-swiper', {
   //   el: '.swiper-pagination',
   // },
   // grabCursor: true,
-    effect: "creative",
-    creativeEffect: {
-      prev: {
-        shadow: true,
-        translate: ["-20%", 0, -1],
-      },
-      next: {
-        translate: ["100%", 0, 0],
-      },
-    },
-  spaceBetween: 30,
+  direction: "vertical",
+  slidesPerView: 1,
+  // spaceBetween: 30,
   mousewheel: true,
   autoplay: {
     delay: 5000,
